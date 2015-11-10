b9157d4e39f1362959812fe8a68afdc3d6e0fcaf
tommy <tommyputranto1@gmail.com>
2015-11-10 17:03:56 +0700

    Initial commit

---
+++ app/src/main/java/com/androbos/navigation/Fragment/FirstFragment.java
@@ -0,0 +1,35 @@
+package com.androbos.navigation.Fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.androbos.navigation.MainActivity;
+import com.androbos.navigation.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FirstFragment extends Fragment {
+
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+         View view =  inflater.inflate(R.layout.fragment_first, container, false);
+        ((MainActivity)getActivity()).getSupportActionBar().setTitle("gambar 1");
+      //  ((FlexibleActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
+        return view;
+    }
+
+}
